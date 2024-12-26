9549f48cf657f5154a2d3f4d9a7b2a47a188836e
Angelina <ali27@Angelinas-Air.modem>
2024-12-26 15:14:04 +1100

    Initial commit with project files

---
+++ Models/Goal.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CommBank.Models;
+
+public class Goal
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string? Name { get; set; }
+
+    // 新增 Icon 字段
+    public string? Icon { get; set; } // 可选字段，存储图标信息
+    
+    public UInt64 TargetAmount { get; set; } = 0;
+
+    public DateTime TargetDate { get; set; }
+
+    public double Balance { get; set; } = 0.00;
+
+    public DateTime Created { get; set; } = DateTime.Now;
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public List<string>? TransactionIds { get; set; }
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public List<string>? TagIds { get; set; }
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? UserId { get; set; }
+}
